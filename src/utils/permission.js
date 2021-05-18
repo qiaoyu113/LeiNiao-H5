d7fc0605881ad7bb583477ae33f62540762f0dc1
cuikaidao <cuikaidao@yunniao.me>
2021-05-18 10:20:40 +0800

    【fix】(权限控制)：控制框架权限控制

--- src/utils/permission.js
+++ src/utils/permission.js
@@ -12,44 +12,17 @@ router.beforeEach(async(to, from, next) => {
   // determine whether the user has logged in
   const hasToken = getToken()
   if (hasToken) {
-    // 已经登录，跳转到首页
-    // next({ path: '/' })
-    // await store.dispatch('user/getInfo')
-    // if (to.matched.length === 1) { // 匹配前往的路由不存在
-    //   if (to.path === '/404') { // 这就是跳出循环的关键
-    //     next()
-    //   } else {
-    //     console.log('from', from)
-    //     from.name ? next({
-    //       name: from.name
-    //     }) : next('/404');
-    //   }
-    // } else {
     if (to.path === '/login') {
       next('/')
     } else {
       next()
     }
-    // }
   } else {
     /* has no token */
     if (whiteList.indexOf(to.path) !== -1) {
       // 白名单中，无需验证
       next()
     } else {
-      // other pages that do not have permission to access are redirected to the login page.
-      // 微信未授权登录跳转到授权登录页面
-      // const url = window.location.href
-      // 解决重复登录url添加重复的code与state问题
-      // const parseUrl = qs.parse(url.split('?')[1])
-      // let loginUrl
-      // if (parseUrl.code && parseUrl.state) {
-      //   delete parseUrl.code
-      //   delete parseUrl.state
-      //   loginUrl = `${url.split('?')[0]}?${qs.stringify(parseUrl)}`
-      // } else {
-      //   loginUrl = url
-      // }
       localStorage.setItem('loginUrl', to.path)
       // 无论拒绝还是授权都设置成1
       next(`/login?redirect=${to.path}`)
